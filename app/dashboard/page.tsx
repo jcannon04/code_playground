fb64536e5d0264ff2e5fb5fb07eb8050f7c0629a
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-09-24 16:50:23 -0500

    Users Roles can be changed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,26 +1,39 @@
 "use client"
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import RoleSelect from "../components/RoleSelect";
 import axios from "axios";
-import { useUser, currentUser } from "@clerk/nextjs";
-const DashBoard = () => {
-//     const { isSignedIn, user, isLoaded } = useUser();
-//    console.log(user);
-    
-//     const {  username } = user;
-//     let userEmail = user.emailAddresses[0].emailAddress
-//     let helloUser = isLoaded ? <div>{user.username}</div> : undefined;
-   
-//     useEffect(() => {
-//         axios.post('./api/Users');
+import { useUser } from "@clerk/nextjs";
+import { Button } from "flowbite-react";
 
-//      })
+const DashBoard = () => {
+    //const { isSignedIn, user, isLoaded } = useUser();
+    const { isSignedIn, user, isLoaded } = useUser();
+    const [openModal, setOpenModal] = useState<string | undefined>();
+  
+
+    async function PostUser() {
+        if (isLoaded) {
+            const response = await axios.post('http://localhost:3000/api/User', { username: user.username, email: user.emailAddresses[0].emailAddress });
+            if (response.data.newUser === true) {
+                setOpenModal("dismissible");
+           }
+            console.log(response.data);
+             console.log(user.username)
+         }
+ 
+    }
+    useEffect(() => {
+        PostUser();
+       
+    }, [user]);
 
     return (
         <>
-            {/* {helloUser} */}
             <div className='flex justify-center items-center h-96'>
+            {/* <Button onClick={() => setOpenModal('dismissible')}>Toggle modal</Button> */}
                 <div className='text-center'>
+
                     <Link href="/create/project">
                         <button className='bg-black hover:bg-slate-600 text-white font-bold py-2 px-4 rounded mr-2'>
                             New Project
@@ -31,6 +44,9 @@ const DashBoard = () => {
                             Browse Projects
                         </button>
                     </Link>
+                    <RoleSelect
+                        openModal={openModal }
+                        setOpenModal={ setOpenModal} />
                 </div>
             </div>
         </>
