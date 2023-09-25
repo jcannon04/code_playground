f33756e9ccf6de85a2c3700f204c3a82758fd242
ConnerKT <connerthompson121@gmail.com>
2023-09-25 09:41:16 -0500

    styling addition

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,23 @@
 import Link from "next/link";
+
 const DemoPage = () => {
   return (
-      <div className="h-full flex items-center justify-center mt-12">
-          <div className="bg-white rounded-lg shadow-lg p-8 max-w-md w-full">
-              <h1 className="text-2xl font-bold mb-4">Welcome to CodeCrewLabs</h1>
-              <p className="mb-6">
-                  CodeCrewLabs is an interactive e-learning environment with a codepen-like web playground.
-                  You can create projects in multiple programming languages, practice coding, and more!
-              </p>
-              <Link href="/demo">
-              <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
-                  Try Now!
-              </button>
-              </Link>
-          </div>
+    
+    <div className="h-full flex items-center justify-center mt-24">
+        
+      <div className="rounded-lg p-8 max-w-md w-full flex flex-col items-center">
+        <h1 className="text-9xl font-bold mb-24 text-white " >Welcome to <span className="border-2 border-blue-300">BitBox</span> </h1>
+        <p className="mb-6 text-white">
+          BitBox is your coding platform and grading platform of the future.
+        </p>
+        <Link href="/demo">
+          <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105 hover:animate-glow">
+            Try Now!
+          </button>
+        </Link>
       </div>
+      
+    </div>
   );
 };
 
