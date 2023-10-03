8786188d68b61bb5f971faafa6f5f4598b8ccc30
ConnerKT <connerthompson121@gmail.com>
2023-10-03 11:36:31 -0500

    pushing style changes

--- app/ParticlesContainer.tsx
+++ app/ParticlesContainer.tsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import Particles from "react-particles";
+import React, { Component, useState } from "react";
+import Particles from "react-tsparticles";
 import type { Engine } from "tsparticles-engine";
 import { loadFirePreset } from "tsparticles-preset-fire";
 
@@ -8,12 +8,15 @@ interface IProps {
 }
 
 class ParticlesContainer extends Component<IProps> {
+
+
   async customInit(engine: Engine): Promise<void> {
     // Load the "fire" preset for tsParticles
     await loadFirePreset(engine);
   }
 
   render() {
+    
     const options = {
       preset: "fire", // Use the "fire" preset
       particles: {
